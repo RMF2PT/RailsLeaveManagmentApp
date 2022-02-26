bfd86634b8fbe55828610ae44f2fda92b10fa6fe
rmf2pt <rmf2pt@gmail.com>
2022-02-26 19:13:40 +0000

    Change all app ti english

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,4 @@
 class Unit < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..30 }, uniqueness: { case_sensitive: false }
+  validates :short_name, presence: true, length: { is: 2 }, uniqueness: { case_sensitive: false }
 end
