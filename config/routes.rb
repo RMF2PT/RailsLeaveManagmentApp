5466211073f6a48f7f4e2a7bf2ba3cceedce3c34
rmf2pt <rmf2pt@gmail.com>
2022-02-27 16:41:27 +0000

    Add users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+  match '/users',   to: 'users#index',   via: 'get'
   resources :teams
   resources :units
   # Defines the root path route ("/")
