86cc700cb18451cd953d77ec3dbcd549c1a7ae0e
rmf2pt <rmf2pt@gmail.com>
2022-02-27 12:58:06 +0000

    Stylized views for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,22 @@
 class Team < ApplicationRecord
   belongs_to :unit
+
+  validates :name, length: { in: 2..30 }, uniqueness: { case_sensitive: false }
+  validates :short_name, length: { is: 2 }, uniqueness: { case_sensitive: false }
+
+  before_save :capitalize_attributes, :uppercase_attributes
+
+  private
+  def capitalize_attributes
+    capitalizable = ["name"]
+    self.attributes.each do |attr,val|
+    self.send("#{attr}=",val.to_s.strip.capitalize) if capitalizable.include?(attr)
+    end
+  end
+  def uppercase_attributes
+    upcase_att = ["short_name"]
+    self.attributes.each do |attr,val|
+    self.send("#{attr}=",val.to_s.strip.upcase) if upcase_att.include?(attr)
+    end
+  end
 end
