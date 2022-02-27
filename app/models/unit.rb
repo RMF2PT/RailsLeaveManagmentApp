b6d61062cfbe620cdd97d9e666ab849fad83a8c8
rmf2pt <rmf2pt@gmail.com>
2022-02-27 08:40:08 +0000

    Remove validate presence because minimum lenght already does that

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,4 +1,4 @@
 class Unit < ApplicationRecord
-  validates :name, presence: true, length: { in: 2..30 }, uniqueness: { case_sensitive: false }
-  validates :short_name, presence: true, length: { is: 2 }, uniqueness: { case_sensitive: false }
+  validates :name, length: { in: 2..30 }, uniqueness: { case_sensitive: false }
+  validates :short_name, length: { is: 2 }, uniqueness: { case_sensitive: false }
 end
