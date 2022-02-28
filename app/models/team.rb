192e01753ba91eb51f4c300d9ae5bf7be62113c7
rmf2pt <rmf2pt@gmail.com>
2022-02-28 15:49:17 +0000

    Clean teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,14 +9,14 @@ class Team < ApplicationRecord
   private
   def capitalize_attributes
     capitalizable = ["name"]
-    self.attributes.each do |attr,val|
-    self.send("#{attr}=",val.to_s.strip.capitalize) if capitalizable.include?(attr)
+    attributes.each do |attr, val|
+      send("#{attr}=", val.to_s.strip.capitalize) if capitalizable.include?(attr)
     end
   end
   def uppercase_attributes
     upcase_att = ["short_name"]
-    self.attributes.each do |attr,val|
-    self.send("#{attr}=",val.to_s.strip.upcase) if upcase_att.include?(attr)
+    attributes.each do |attr, val|
+      send("#{attr}=", val.to_s.strip.upcase) if upcase_att.include?(attr)
     end
   end
 end
